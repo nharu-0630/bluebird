9437e38f6837d9efe820d767642256cc1ebab83f
nharu-0630 <8305330+nharu-0630@users.noreply.github.com>
2024-07-03 13:29:16 +0900

    [fix] モックデータのIDを修正

--- backend/mock/shelf.go
+++ backend/mock/shelf.go
@@ -1,27 +1,28 @@
 package mock
 
 import (
+	"github.com/nharu-0630/bluebird/config"
 	"github.com/nharu-0630/bluebird/model"
 	"github.com/oklog/ulid/v2"
 )
 
 func MockShelfCategory() []model.ShelfCategory {
 	return []model.ShelfCategory{
-		{Ulid: ulid.Make().String(), Name: "同人誌"},
-		{Ulid: ulid.Make().String(), Name: "漫画"},
+		{Ulid: config.ShelfCategoryIDPrefix + ulid.Make().String(), Name: "同人誌"},
+		{Ulid: config.ShelfCategoryIDPrefix + ulid.Make().String(), Name: "漫画"},
 	}
 }
 
 func MockShelfTag() []model.ShelfTag {
 	return []model.ShelfTag{
-		{Ulid: ulid.Make().String(), Name: "１次創作"},
-		{Ulid: ulid.Make().String(), Name: "２次創作"},
+		{Ulid: config.ShelfTagIDPrefix + ulid.Make().String(), Name: "１次創作"},
+		{Ulid: config.ShelfTagIDPrefix + ulid.Make().String(), Name: "２次創作"},
 	}
 }
 
 func MockShelfLocation() []model.ShelfLocation {
 	return []model.ShelfLocation{
-		{Ulid: ulid.Make().String(), Name: "同人誌ボックス"},
-		{Ulid: ulid.Make().String(), Name: "本棚"},
+		{Ulid: config.ShelfLocationIDPrefix + ulid.Make().String(), Name: "同人誌ボックス"},
+		{Ulid: config.ShelfLocationIDPrefix + ulid.Make().String(), Name: "本棚"},
 	}
 }
