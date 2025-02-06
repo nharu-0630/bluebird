3f6bc8c0e1b950bdd39d7e30d3a80e37ea48f221
nharu-0630 <8305330+nharu-0630@users.noreply.github.com>
2025-02-06 17:01:31 +0900

    [add] ユーザカードのバナー画像を追加

--- frontend/src/app/twitter/(components)/common/user-card.tsx
+++ frontend/src/app/twitter/(components)/common/user-card.tsx
@@ -15,63 +15,71 @@ export function UserCard({ item }: UserCardProps) {
     <div
       key={item.id}
       className={
-        "flex flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent m-2 max-w-96"
+        "flex flex-col items-start gap-2 rounded-lg border text-left text-sm transition-all hover:bg-accent m-2 max-w-96"
       }
     >
-      <div className="flex flex-1 gap-2 flex-wrap items-center justify-start text-sm">
-        <Button
-          variant="link"
-          className="flex gap-2 p-0"
-          onClick={() => router.push(`/twitter/${item?.screenName}`)}
-        >
-          <Avatar className="flex-shrink-0">
-            <AvatarImage src={item?.profileImageURL!} />
-          </Avatar>
-          <div className="flex flex-col items-start justify-center">
-            <div className="font-semibold truncate">{item?.name}</div>
-            <div className="text-xs font-medium">@{item?.screenName}</div>
+      {item.profileBannerURL && (
+        <div
+          className="w-full h-24 rounded-t-lg bg-cover bg-center"
+          style={{ backgroundImage: `url(${item.profileBannerURL})` }}
+        />
+      )}
+      <div className="flex flex-col items-start gap-2 p-3 m-2">
+        <div className="flex flex-1 gap-2 flex-wrap items-center justify-start text-sm">
+          <Button
+            variant="link"
+            className="flex gap-2 p-0"
+            onClick={() => router.push(`/twitter/${item?.screenName}`)}
+          >
+            <Avatar className="flex-shrink-0">
+              <AvatarImage src={item?.profileImageURL!} />
+            </Avatar>
+            <div className="flex flex-col items-start justify-center">
+              <div className="font-semibold truncate">{item?.name}</div>
+              <div className="text-xs font-medium">@{item?.screenName}</div>
+            </div>
+          </Button>
+          <div className="flex flex-wrap items-center justify-around text-sm">
+            <Button
+              variant="link"
+              onClick={() =>
+                window.open(`/twitter/${item?.screenName}/followers`)
+              }
+            >
+              <span className="text-xs">
+                <span className="font-semibold">
+                  {item?.followersCount?.toLocaleString()}
+                </span>{" "}
+                Followers
+              </span>
+            </Button>
+            {item?.following && <Badge variant={"outline"}>Following</Badge>}
+            <Button
+              variant="link"
+              onClick={() =>
+                window.open(`/twitter/${item?.screenName}/following `)
+              }
+            >
+              <span className="text-xs">
+                <span className="font-semibold">
+                  {item?.friendsCount?.toLocaleString()}
+                </span>{" "}
+                Following
+              </span>
+            </Button>
+            {item?.followedBy && <Badge variant={"outline"}>FollowedBy</Badge>}
           </div>
-        </Button>
-        <div className="flex flex-wrap items-center justify-around text-sm">
-          <Button
-            variant="link"
-            onClick={() =>
-              window.open(`/twitter/${item?.screenName}/followers`)
-            }
-          >
-            <span className="text-xs">
-              <span className="font-semibold">
-                {item?.followersCount?.toLocaleString()}
-              </span>{" "}
-              Followers
-            </span>
-          </Button>
-          {item?.following && <Badge variant={"outline"}>Following</Badge>}
-          <Button
-            variant="link"
-            onClick={() =>
-              window.open(`/twitter/${item?.screenName}/following `)
-            }
-          >
-            <span className="text-xs">
-              <span className="font-semibold">
-                {item?.friendsCount?.toLocaleString()}
-              </span>{" "}
-              Following
-            </span>
-          </Button>
-          {item?.followedBy && <Badge variant={"outline"}>FollowedBy</Badge>}
         </div>
-      </div>
-      <div className="text-s">
-        <span>{item.description}</span>
-      </div>
-      <div className="flex flex-1 flex-wrap gap-2 justify-end items-center w-full">
-        <div>
-          <pre className="text-xs">{item.id}</pre>
-          <pre className="text-xs">
-            {item.createdAt && new Date(item.createdAt).toLocaleString()}
-          </pre>
+        <div className="text-s">
+          <span>{item.description}</span>
+        </div>
+        <div className="flex flex-1 flex-wrap gap-2 justify-end items-center w-full">
+          <div>
+            <pre className="text-xs">{item.id}</pre>
+            <pre className="text-xs">
+              {item.createdAt && new Date(item.createdAt).toLocaleString()}
+            </pre>
+          </div>
         </div>
       </div>
     </div>
